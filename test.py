32eb7fd3bb4208dac4ec3bbb159d0289f330214e
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-01 20:43:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import time
-
+st.write('Test started')
 @st.cache
 def expensive_computation(a, b):
-    time.sleep(2)  # This makes the function take 2s to run
+    time.sleep(6)  # This makes the function take 2s to run
     return a * b
 
 a = 2
